7b91feadc1d807648702a533795cff520d88e934
Bas de Reus <dereusbas461@gmail.com>
2024-12-08 22:54:00 +0100

    Start on planting grain

---
+++ Assets/Scripts/Framework/Gameplay/Farming/DirtPath.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Framework.Gameplay.Farming
+{
+    public sealed class DirtPath : Interactable
+    {
+        [SerializeField] private Grain grain;
+
+        private void Start()
+        {
+            grain.gameObject.SetActive(false);
+        }
+
+        public override void DoInteraction()
+        {
+            grain.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Framework/Gameplay/Farming/Grain.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Framework.Gameplay.Farming
+{
+    public sealed class Grain : MonoBehaviour
+    {
+        public void StartGrowing()
+        {
+            Debug.Log("Grow");
+        }
+    }
+}
